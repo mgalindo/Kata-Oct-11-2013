7e462326e15ee660372410b61dfbac8d9274dca8
Miguel Galindo <mgalindo@commandalkon.com>
2013-10-11 14:43:20 -0500

    First version

--- js/middle3.js
+++ js/middle3.js
@@ -1,8 +1,24 @@
+function isInt(n) {
+	return typeof n === 'number' && parseFloat(n) == parseInt(n, 10) && !isNaN(n);
+}
+
 exports.getMiddle3 = function(number, callback){
 
-	//callback(null, number);
+	if (!isInt(number)) {
+		callback(new Error(number+" is not an integer"));
+		return;
+	};
 
-	callback(new Error("Can't divide by zero"));
+	var strnumber = number.toString();
+
+	if (strnumber.length < 5) {
+		callback(new Error(number+" is not long enough"));
+		return;
+	}
+
+	result = strnumber.substring((strnumber.length/2)-1, (strnumber.length/2)+2)
+	callback(null, result);
+
 
 };
 
--- js/testmiddle3.js
+++ js/testmiddle3.js
@@ -1,15 +1,131 @@
 var middle3 = require("./middle3.js");
 
-var number = 123;
+var number = 'aaa';
 middle3.getMiddle3(number, function(err, result){
 	if (err){
 	  console.log('Error getting the 3 middle digits', err)
 	}
 	else{
-		console.log('The middle 3 digits are: '+result);
+		console.log('For '+number+' the middle 3 digits are: '+result);
 	};
 
 });
 
 
+number = 123.25;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
 
+});
+
+
+number = 12;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
+
+});
+
+
+number = 123;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
+
+});
+
+
+number = 1234;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
+
+})
+
+
+
+number = 12345;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
+
+})
+
+number = 123456;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
+
+})
+
+
+number = 1234567;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
+
+})
+
+
+number = 12345678;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
+
+})
+
+
+number = 123456789;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
+
+})
+
+number = 1234567891;
+middle3.getMiddle3(number, function(err, result){
+	if (err){
+		console.log('Error getting the 3 middle digits', err)
+	}
+	else{
+		console.log('For '+number+' the middle 3 digits are: '+result);
+	};
+
+})
